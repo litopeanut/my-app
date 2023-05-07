3b2dedfff9cbb50728df101ee3152c37a57a1521
Angie <spockism@gmail.com>
2023-05-06 20:18:33 -0700

    Updated classname

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,16 +1,15 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col-1">
-          <div className="WeatherForecast-day">Thu</div>
-          <div className="WeatherForecast-icon">
-            <WeatherIcon code="13d" />
-          </div>
+          <div className="WeatherForecastDay">Thu</div>
+          <WeatherIcon code="01d" size={34} />{" "}
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
